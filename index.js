c355e3ba0cd273d1e1818ddbd34f6be5b28c6efb
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-17 16:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-let func1 = function wakeDog(dogName, dogBreed) {
+function wakeDog(dogName, dogBreed) {
   let string = `Wake ${dogName} the ${dogBreed}` ;
   console.log(string);
   return string ;
-};
-let func2 =function leashDog(dogName, dogBreed) {
+}
+function leashDog(dogName, dogBreed) {
   let string = `Leash ${dogName} the ${dogBreed}` ;
   console.log(string);
     return string ;
-};
-
-let func3 = function walkToPark(dogName, dogBreed) {
+}
+function walkToPark(dogName, dogBreed) {
   let string = `Walk to the park with ${dogName} the ${dogBreed}` ;
   console.log(string);
     return string ;
-};
-let func4 =function throwFrisbee(dogName, dogBreed) {
+}
+function throwFrisbee(dogName, dogBreed) {
   let string = `Throw the frisbee for ${dogName} the ${dogBreed}` ;
   console.log(string);
     return string ;
-};
-let func5 =function walkHome(dogName, dogBreed) {
+}
+function walkHome(dogName, dogBreed) {
   let string = `Walk home with ${dogName} the ${dogBreed}` ;
   console.log(string);
     return string ;
-};
-let func6 =function unleashDog(dogName, dogBreed) {
+}
+function unleashDog(dogName, dogBreed) {
   let string = `Unleash ${dogName} the ${dogBreed}` ;
   console.log(string);
     return string ;
-};
+}
 
 let routine = [wakeDog() ,leashDog() ,walkToPark() ,throwFrisbee()
 ,walkHome(),unleashDog()] ;
